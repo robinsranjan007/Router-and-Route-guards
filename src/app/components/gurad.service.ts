6458ba577202bb99babf6a25e26b42643390febe
robinsranjan <robinsranjan007@gmail.com>
2024-03-30 00:22:28 +0530

    routeguard

--- src/app/components/gurad.service.ts
+++ src/app/components/gurad.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanDeactivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ContactComponent } from './contact/contact.component';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GuradService implements CanActivate,CanActivateChild{
+export class GuradService implements CanActivate,CanActivateChild,CanDeactivate<ContactComponent>{
 
 constructor(private guardservices:AuthService,private route:Router){}
-
+ 
   canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot):boolean
   {
     if(this.guardservices.isAuthenticated())
@@ -27,8 +28,16 @@ constructor(private guardservices:AuthService,private route:Router){}
   }
 
     canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+      
       return this.canActivate(childRoute,state);
+
     }
 
+    canDeactivate(component:ContactComponent,route:ActivatedRouteSnapshot,state:RouterStateSnapshot,next:RouterStateSnapshot):boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>
+    {
+      return false;
+    }
+
+     
 
 }
